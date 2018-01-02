5032a632e86b7b793a24cadf37e2fab1c9a828d4
Tomáš Krejčí <tomas@differencemakers.cz>
2018-01-02 05:52:21 +0100

    .

--- src/Bridge/Connector/AbstractUploadDataHandler.php
+++ src/Bridge/Connector/AbstractUploadDataHandler.php
@@ -23,4 +23,35 @@ abstract class AbstractUploadDataHandler extends \Sellastica\Connector\Model\Abs
 	{
 		return isset($string) ? Strings::upper(Strings::substring(str_replace('-', '', $string), 0, 20)) : null;
 	}
+
+	/**
+	 * @param mixed $value
+	 * @return mixed
+	 */
+	protected function convertValue($value)
+	{
+		if (is_null($value)) {
+			return '';
+		} elseif ($value === true) {
+			return 'true';
+		} elseif ($value === false) {
+			return 'false';
+		}
+
+		return $value;
+	}
+
+	/**
+	 * @param array $data
+	 * @return array
+	 */
+	protected function convertAllValues(array $data): array
+	{
+		$convertedData = [];
+		foreach ($data as $key => $value) {
+			$convertedData[$key] = $this->convertValue($value);
+		}
+
+		return $convertedData;
+	}
 }
